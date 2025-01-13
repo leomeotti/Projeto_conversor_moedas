259376562c2ce626f86f932f18a87b11df91309d
leomeotti <100806613+leomeotti@users.noreply.github.com>
2025-01-13 15:54:23 -0300

    Add files via upload

---
+++ Conversor_Moedas/pegar_moeda.py
@@ -0,0 +1,23 @@
+import xmltodict 
+
+def nomes_moedas():
+    with open("c:/Users/Leo meotti/Desktop/Codigos Python/Conversor_Moedas/moedas.xml", "rb") as arquivo_moedas:
+        dic_moedas = xmltodict.parse(arquivo_moedas)
+
+    moedas = dic_moedas["xml"]
+    return moedas
+
+
+def conversoes_disponoveis():
+    with open("c:/Users/Leo meotti/Desktop/Codigos Python/Conversor_Moedas/conversoes.xml", "rb") as arquivo_conversoes:
+        dic_conversoes = xmltodict.parse(arquivo_conversoes)
+
+    conversoes = dic_conversoes["xml"]
+    dic_conversoes_disponiveis = {}
+    for par_conversao in conversoes:
+        moeda_origem, moeda_destino = par_conversao.split("-")
+        if moeda_origem in dic_conversoes_disponiveis:
+            dic_conversoes_disponiveis[moeda_origem].append(moeda_destino)
+        else:
+            dic_conversoes_disponiveis[moeda_origem] = [moeda_destino]
+    return dic_conversoes_disponiveis
